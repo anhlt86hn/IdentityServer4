5614f6416b81269874702932e28b7ad1f445f4a6
Anh Luong Tuan <anh.luongtuan@ncc.asia>
2020-03-17 17:37:54 +0700

    OAuth Client (P.1)

---
+++ Authentication/Client/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Client
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(config => {
+                // we check the cookie to confirm that we are authenticated
+                config.DefaultAuthenticateScheme = "ClientCookie";
+                // when we sign in we will deal out a cookie
+                config.DefaultSignInScheme = "ClientCookie";
+                // use this to check if we are allowed to do something.
+                config.DefaultChallengeScheme = "OutServer";
+            })
+                .AddCookie("ClientCookie")
+                .AddOAuth("OurServer", config =>
+                {
+                    config.ClientId = "client_id";
+                    config.ClientSecret = "client_secret";
+                    config.CallbackPath = "/oauth/callback";
+                    config.AuthorizationEndpoint = "https://localhost:5001/oauth/authorize";
+                    config.TokenEndpoint = "https://localhost:5001/oauth/token";
+                });
+            services.AddControllersWithViews();
+        }
+
+       
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
---
+++ Authentication/Server/Controllers/OAuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    public class OAuthController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Authorize(
+            string response_type, // authorization flow type
+            string client_id, // client id
+            string redirect_uri, 
+            string scope, // what info I want = email, grandma, tel
+            string state) // random string generated to confirm that we are going to back the same client
+        {
+            // ?a=foo&b=bar
+            var query = new QueryBuilder();
+            query.Add("redirectUri", redirect_uri);
+            query.Add("state", state);
+            return View(model: query.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult Authorize(
+            string username,           
+            string redirect_uri,          
+            string state)
+        {
+            const string code = "BABABA";
+            return Redirect("");
+        }
+
+        public IActionResult Token()
+        {
+            return View();
+        }
+
+    }
+}
