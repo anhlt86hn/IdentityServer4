c8c7cddcf2a507e371beb50ad3196a4a4796ea48
Luong Anh <luonganh@gmail.com>
2020-03-17 22:43:35 +0700

    OAuth Client (P.2)

--- Authentication/Client/Startup.cs
+++ Authentication/Client/Startup.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace Client
 {
@@ -29,10 +33,31 @@ namespace Client
                     config.ClientId = "client_id";
                     config.ClientSecret = "client_secret";
                     config.CallbackPath = "/oauth/callback";
-                    config.AuthorizationEndpoint = "https://localhost:5001/oauth/authorize";
-                    config.TokenEndpoint = "https://localhost:5001/oauth/token";
+                    config.AuthorizationEndpoint = "https://localhost:44325/oauth/authorize";
+                    config.TokenEndpoint = "https://localhost:44325/oauth/token";
+
+                    config.SaveTokens = true;
+                    config.Events = new OAuthEvents()
+                    {
+                        OnCreatingTicket = context =>
+                        {
+                            var accessToken = context.AccessToken;
+                            var base64payload = accessToken.Split('.')[1];
+                            var bytes = Convert.FromBase64String(base64payload);
+                            var jsonPayload = Encoding.UTF8.GetString(bytes);
+                            var claims = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonPayload);
+
+                            foreach (var claim in claims)
+                            {
+                                context.Identity.AddClaim(new Claim(claim.Key, claim.Value));
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
                 });
-            services.AddControllersWithViews();
+
+            services.AddControllersWithViews().AddRazorRuntime();
         }
 
        
--- Authentication/Server/Constants.cs
+++ Authentication/Server/Constants.cs
@@ -8,7 +8,7 @@ namespace Server
 {
     public static class Constants
     {
-        public const string Audiance = "https://localhost:5001/";
+        public const string Audiance = "https://localhost:44325/";
         public const string Issuer = Audiance;
         public const string Secret = "not_too_short_secret_otherwise_it_might_error";
     }
